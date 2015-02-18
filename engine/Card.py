cf1d760ec80ac0b94ed55e184845b7c742f3e923
Raymond <raymond@Raymonds-MBP.home>
2015-02-17 22:55:23 -0500

    Updated asserts in Card.py

--- engine/Card.py
+++ engine/Card.py
@@ -28,8 +28,8 @@ class Card:
     def __init__(self, rank, suit):
         assert isinstance(rank, int), "rank is not an int"
         assert isinstance(suit, int), "suit is not an int"
-        assert rank >= 1 and rank <= 13
-        assert suit >= 0 and suit <= 3
+        assert rank >= 1 and rank <= 13, "rank must be 1-13"
+        assert suit >= 0 and suit <= 3, "suit must be 0-3"
         self.rank = rank
         self.suit = suit
 
