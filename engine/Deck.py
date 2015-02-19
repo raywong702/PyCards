459de758166efd5e1bd2dfc4e7f981ab6c053a48
Raymond <raymond@Raymonds-MacBook-Pro.local>
2015-02-18 22:11:19 -0500

    Added shuffle, asserts, and docstrings to Deck.py

---
+++ engine/Deck.py
@@ -0,0 +1,28 @@
+from random import shuffle as shuf
+import Card as c
+
+class Deck(object):
+    """Represents one or more deck of cards
+
+    Attributes:
+        pile: the deck of cards. Defaults to 1 deck of cards, but can be more.
+    """
+
+    def __init__(self, num_of_decks = 1):
+        """Creates the deck of cards
+
+        @param num_of_decks is the amount of 52 card decks you want
+        defaults to one 52 card deck
+        """
+
+        assert isinstance(num_of_decks, int), "num_of_decks is not an int"
+        assert num_of_decks > 0, "num_of_decks must be greater than 0"
+        self.pile = [c.Card(r, s) for r in range(1, 14) for s in range(4)] * num_of_decks
+
+    def __str__(self):
+        """Return human readable representation of one or more deck of cards"""
+        return str([str(card) for card in self.pile])
+
+    def shuffle(self):
+        """Shuffles the deck"""
+        shuf(self.pile)
