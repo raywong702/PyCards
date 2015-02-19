9ac643778885fa40182de72a3166af7d8affffa4
Raymond <raymond@Raymonds-MacBook-Pro.local>
2015-02-19 00:08:55 -0500

    Privatize pile and PEP8 line length.

--- engine/Deck.py
+++ engine/Deck.py
@@ -15,17 +15,21 @@ class Deck(object):
         defaults to one 52 card deck
         """
 
-        assert isinstance(num_of_decks, int), "num_of_decks is not an int"
-        assert num_of_decks >= 0, "num_of_decks must be greater than 0"
-        self.pile = [c.Card(r, s) for r in range(1, 14) for s in range(4)] * num_of_decks
+        __ERROR1 = "num_of_decks, '{}', is not an int".format(num_of_decks)
+        __ERROR2 = ("num_of_decks, '{}', must be"
+                    "greater than 0").format(num_of_decks)
+        assert isinstance(num_of_decks, int), __ERROR1
+        assert num_of_decks >= 0, __ERROR2
+        self.__pile = [c.Card(r, s) for r in range(1, 14) for s in range(4)]
+        self.__pile *= num_of_decks
 
     def __str__(self):
         """Return human readable representation of one or more deck of cards"""
-        return str([str(card) for card in self.pile])
+        return str([str(card) for card in self.__pile])
 
     def shuffle(self):
         """Shuffles the deck"""
-        shuf(self.pile)
+        shuf(self.__pile)
 
 if __name__ == '__main__':
     pass
