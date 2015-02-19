3b503460f272682001fb080ae368f3c636172f90
Raymond <raymond@Raymonds-MacBook-Pro.local>
2015-02-19 01:25:10 -0500

    Added accessor methods to Deck.py

--- engine/Deck.py
+++ engine/Deck.py
@@ -16,10 +16,12 @@ class Deck(object):
         """
 
         __ERROR1 = "num_of_decks, '{}', is not an int".format(num_of_decks)
-        __ERROR2 = ("num_of_decks, '{}', must be"
+        __ERROR2 = ("num_of_decks, '{}', must be "
                     "greater than 0").format(num_of_decks)
+
         assert isinstance(num_of_decks, int), __ERROR1
         assert num_of_decks >= 0, __ERROR2
+
         self.__pile = [c.Card(r, s) for r in range(1, 14) for s in range(4)]
         self.__pile *= num_of_decks
 
@@ -28,8 +30,56 @@ class Deck(object):
         return str([str(card) for card in self.__pile])
 
     def shuffle(self):
-        """Shuffles the deck"""
+        """Shuffles the deck."""
         shuf(self.__pile)
+
+    def find(self, card):
+        """Find index of a specic Card in deck. Return -1 if not found"""
+
+        ERROR1 = "card, '{}', is not a Card type".format(card)
+        assert isinstance(card, c.Card), ERROR1
+
+        index = 0
+        rank = card.getRank()
+        suit = card.getSuit()
+
+        for e in self.__pile:
+            c_rank = e.getRank()
+            c_suit = e.getSuit()
+
+            if c_rank == rank and c_suit == suit:
+                return index
+            index += 1
+        return -1
+        
+    def append(self, card):
+        """Add a card to the deck.
+       
+        @param instance of the Card class
+        """
+
+        ERROR1 = "card, '{}', is not a Card type".format(card)
+        assert isinstance(card, c.Card), ERROR1
+
+        self.__pile.append(card)
+
+    def pop(self):
+        """Draw a card from the the deck."""
+
+        return self.__pile.pop()
+
+    def popCard(self, card):
+        """Remove a specific card from the deck."""
+
+        ERROR1 = "card, '{}', is not a Card type".format(card)
+        assert isinstance(card, c.Card), ERROR1
+
+        index = self.find(card)
+        if index == -1:
+            return None
+        else:
+            return self.__pile.pop(index) 
+
 
 if __name__ == '__main__':
     pass
