72126183c644f7b001e4b04764499614eb12ebd8
Raymond <r.wong@live.com>
2015-02-20 18:46:10 -0500

    Added Player.py

---
+++ engine/Player.py
@@ -0,0 +1,29 @@
+import Deck as d
+
+class Player(object):
+    """Represents a player
+
+    Attributes:
+        name: player's name. string type.
+        hand: player's hand. Deck type.
+    """
+    
+    def __init__(self, name, hand):
+        assert isinstance(name, str), "name '{}' is not a string".format(name)
+        assert isinstance(hand, d.Deck), ("hand '{}' is not an instance "
+                                        "of Deck").format(hand)
+        assert name, "name is empty string"
+
+        self.__name = name
+        self.hand = hand
+
+    def __str__(self):
+        """Return human readable representation"""
+        # Put stuff here
+
+    def getName(self):
+        """Return player's name"""
+        return self.__name
+
+if __name__ == '__main__':
+    pass
